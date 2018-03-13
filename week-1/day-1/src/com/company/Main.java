8122af5ba45307c90f8cf7429e55852cd9a4851d
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-03-12 22:18:16 -0700

    [WIP] Day 1 take home assignment. Does not include practice assignment from module

---
+++ week-1/day-1/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    /* Write a function that takes in a word. This function should return true if the word is a palindrome and false if it is not.
+    A string is considered a palindrome if it remains unchanged when reversed. For example, "dad" is a palindrome as reverse of
+    "dad" is "dad", whereas "program" is not a palindrome. Note: palindromes are case insensitive ("Dad" and "dad" are both palindromes). */
+
+
+    /*Write a function that accepts no parameters and creates a list of any characters and returns a string, which contains every
+    other element in the list. Thus, if the list has 'a', 'b', 'c', 'd', the output should be "bd". */
+
+
+    /* Write a function that takes in an array of integers and returns the max value in that array. Please do not sort the array. */
+
+
+    /* Write a function that takes in a month of the year and returns the number of days in this month. If the input is not a valid month, return 0. */
+
+
+    /* Write a function that takes in an array of integers, sums the integers that are greater than 1, and subtracts 3 if the number is greater
+     than 20. The function returns the result of the operations on the array. */
+
+
+    /* Write a function that takes in a list of names, filters the list removing all duplicate names and returns a list with unique names. */
+
+
+
+    /* Write a function that takes in a string, omits all vowels and returns a new string that contains only consonants. Make sure
+     to catch the edge cases (e.g. empty string, upper/lower case). */
+
+
+    /* Write in a function that takes in a positive integer, creates a linked list with integers in the range from 1 to the input
+     number and returns a middle element of the linked list if the length of the linked list is an odd number, otherwise -1. */
+
+
+    /* Write a function that creates the right data structure to keep the names of your family members and their age. The function
+     asks the user to type in a family member's name and returns his/her age if the name exists or the string "Such family member
+     does not exist!" if such a member does not exit. */
+
+
+    /* Write a function that takes in a list of countries, swaps the first country with the last one in the list and prints out
+     each of them following the new order. Choose the best collection that gives you access to the first and last element. */
+    
+    }
+}
